66e22fd09d134364775be02ea9cfeb5d4b28b0a8
Stephen <stefanodinucci@gmail.com>
2021-02-28 00:03:59 +0100

    GrphQL

---
+++ GraphQLServer/Models/Query.cs
@@ -0,0 +1,17 @@
+﻿using HotChocolate.Types;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GraphQLServer
+{
+    public class Query
+    {
+        public List<Book> GetBook() => new List<Book> 
+        {
+            new Book {Title = "Test", Id = 1, Price = 10 },
+            new Book {Title = "Test2", Id = 2, Price = 112 },
+            new Book {Title = "Test3", Id = 3, Price = 13 },
+            new Book {Title = "Test4", Id = 4, Price = 14 },
+        };
+    }
+}
--- GraphQLServer/Startup.cs
+++ GraphQLServer/Startup.cs
@@ -1,3 +1,5 @@
+using HotChocolate;
+using HotChocolate.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -16,43 +18,23 @@ namespace GraphQLServer
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "GraphQLServer", Version = "v1" });
-            });
+            services
+                .AddGraphQLServer()
+                .AddQueryType<Query>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GraphQLServer v1"));
+                app.UsePlayground();
             }
-
-            app.UseHttpsRedirection();
-
             app.UseRouting();
-
-            app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapGraphQL();
             });
         }
     }
